165b640733625ae49649c8ea67169f76558b2faf
Meghana <meghanamanchal@gmail.com>
2025-06-06 11:44:32 +0530

    Add user authentication: bcrypt, sessions, and auth routes

---
+++ book_buddy/app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+   redirect_to dashboard_path if session[:user_id]
+  end
+
+
+  def create
+  user = User.find_by(email: params[:email])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect_to user_path(user), notice: "Logged in!"
+  else
+    flash.now[:alert] = "Invalid email or password"
+    render :new
+  end
+end
+
+def destroy
+  reset_session
+  redirect_to login_path, notice: "Logged out!"
+end
+
+end
--- book_buddy/app/controllers/users_controller.rb
+++ book_buddy/app/controllers/users_controller.rb
@@ -1,70 +1,48 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :require_login, only: [:show, :dashboard, :edit, :index]
 
-  # GET /users or /users.json
-  def index
-    @users = User.all
-  end
-
-  # GET /users/1 or /users/1.json
-  def show
-  end
-
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
-  def edit
-  end
-
-  # POST /users or /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to @user, notice: "Account created successfully!"
+    else
+      puts @user.errors.full_messages 
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /users/1 or /users/1.json
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: "User updated successfully!"
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /users/1 or /users/1.json
-  def destroy
-    @user.destroy!
+  def index
+    @users = User.all
+  end
 
-    respond_to do |format|
-      format.html { redirect_to users_path, status: :see_other, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
-    end
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def dashboard
+    @user = User.find(session[:user_id])
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.expect(user: [ :name, :email, :password_digest ])
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
